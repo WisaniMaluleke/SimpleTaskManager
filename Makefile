c4488196b9c3e33f5f5fdb25de507c87ba818887
Wisani Maluleke <wisani@genex.co.za>
2025-08-14 11:42:01 +0200

    DEV VITE for hot reload

--- Makefile
+++ Makefile
@@ -1,7 +1,11 @@
 build:
 	docker-compose build
+
 run:
 	docker-compose up --remove-orphans -d
+
+runvite:
+	docker exec simple-task-manager bash -c "npm run dev"
 
 down:
 	docker-compose down
@@ -20,11 +24,15 @@ clear:
 
 permissions:
 	chmod 777 ~/development/learning/SimpleTaskManager && sudo chown wislim:wislim ~/development/learning/SimpleTaskManager
+
 storagelink:
 	docker exec crawler bash -c "php artisan storage:link"
+
 storagePermission:
 	sudo chmod -R 777 storage
+
 con:
 	docker exec -ti crawler bash
+
 stan:
 	./vendor/bin/phpstan analyse
--- vite.config.js
+++ vite.config.js
@@ -3,18 +3,24 @@ import laravel from 'laravel-vite-plugin';
 import vue from '@vitejs/plugin-vue';
 
 export default defineConfig({
+    server: {
+        host: '0.0.0.0',      // expose to Docker network
+        port: 5173,           // fixed port for HMR
+        strictPort: true,
+        watch: {
+            usePolling: true, // required for Docker
+        },
+        hmr: {
+            host: 'localhost', // or your host machine IP
+            protocol: 'ws',    // use WebSocket
+            port: 5173,
+        },
+    },
     plugins: [
         laravel({
-            input: 'resources/js/app.js',
+            input: ['resources/css/app.css', 'resources/js/app.js'],
             refresh: true,
         }),
-        vue({
-            template: {
-                transformAssetUrls: {
-                    base: null,
-                    includeAbsolute: false,
-                },
-            },
-        }),
+        vue(), // <-- add this
     ],
 });
